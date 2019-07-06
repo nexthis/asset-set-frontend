21170ed1888dab84d782d9c6d6c7f071d7b8c727
nexthis <36706292+pawcioo@users.noreply.github.com>
2019-07-06 18:06:25 +0200

    update GlobalStyle

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,16 +1,35 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  *, *::before, *::after {
+
+  html {
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+  strong, b {
+    font-weight: bolder;
+  }
   body {
+    color: rgba(0, 0, 0, 0.87);
     margin: 0;
-    font-family: 'Roboto', sans-serif;
+    font-size: 0.875rem;
+    font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+    font-weight: 400;
+    line-height: 1.43;
+    letter-spacing: 0.01071em;
+    background-color: #fafafa;
+  }
+  @media print {
+    body {
+      background-color: #fff;
+    }
   }
 `;
+
+// 1rem = 16px
 
 export default GlobalStyle;
