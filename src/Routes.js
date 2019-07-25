517897d6ad8fe8d82db39a2b87627a976ce38fac
nexthis <36706292+pawcioo@users.noreply.github.com>
2019-07-25 18:46:26 +0200

    Code Splitting

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
@@ -6,18 +6,21 @@ import theme from 'theme/theme';
 import GlobalStyle from 'theme/GlobalStyle';
 import { StylesProvider } from '@material-ui/styles';
 
-import LandingPage from 'Pages/LandingPage/LandingPage';
-import HomePage from 'Pages/HomePage/HomePage';
+const LandingPage = lazy(() => import('Pages/LandingPage/LandingPage'));
+const HomePage = lazy(() => import('Pages/HomePage/HomePage'));
 
 const Routes = () => (
   <>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <StylesProvider injectFirst>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/home" component={HomePage} />
-        </Switch>
+        {/* lazy loading / Code Splitting */}
+        <Suspense fallback={null}>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/home" component={HomePage} />
+          </Switch>
+        </Suspense>
       </StylesProvider>
     </ThemeProvider>
   </>
